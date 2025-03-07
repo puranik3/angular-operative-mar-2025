278419db1d867d4d399d43f5c3001d3ff28814fc
Prashanth Puranik <puranik@digdeeper.in>
2025-03-07 18:10:41 +0530

    step 3

---
+++ demos/03-angular/path.md
@@ -0,0 +1,4 @@
+Please add this to the system PATH / user PATH variable
+C:\Users\<userfolder>\AppData\Roaming\npm
+
+If you do not have `npm` folder, create it within `C:\Users\<userfolder>\AppData\Roaming`
---
+++ demos/03-angular/workshops-app/src/app/app.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+// import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { NgbAlertModule } from '@ng-bootstrap/ng-bootstrap';
+
+@Component({
+  selector: 'app-root',
+  standalone: true, // not part of any module (since Angular 17)
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
+  imports: [
+    NgbAlertModule, // alert component
+    CommonModule, // *ngIf directive
+  ],
+})
+export class AppComponent {
+  title = 'Workshops App';
+
+  changeTitle() {
+    this.title = 'Workshops Application';
+  }
+}
