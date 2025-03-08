32076337d676d804242579970534f8bb9bde42f5
Prashanth Puranik <puranik@digdeeper.in>
2025-03-08 12:46:12 +0530

    STEP 7

--- demos/03-angular/workshops-app/src/app/menu/menu.component.ts
+++ demos/03-angular/workshops-app/src/app/menu/menu.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-menu',
   standalone: true,
-  imports: [NgbModule],
+  imports: [NgbModule, RouterLink, RouterLinkActive],
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.scss',
 })
