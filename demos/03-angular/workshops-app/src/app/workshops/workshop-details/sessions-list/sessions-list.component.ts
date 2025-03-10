15e59444ff27e9aa4282c981911257ccf502fc2f
Prashanth Puranik <puranik@digdeeper.in>
2025-03-10 14:47:35 +0530

    Step 25: Fetched list of sessions

--- demos/03-angular/workshops-app/src/app/workshops/workshop-details/sessions-list/sessions-list.component.ts
+++ demos/03-angular/workshops-app/src/app/workshops/workshop-details/sessions-list/sessions-list.component.ts
@@ -1,12 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { SessionsService } from '../../sessions.service';
+import ISession from '../../models/ISession';
 
 @Component({
   selector: 'app-sessions-list',
   standalone: true,
   imports: [],
   templateUrl: './sessions-list.component.html',
-  styleUrl: './sessions-list.component.scss'
+  styleUrl: './sessions-list.component.scss',
 })
-export class SessionsListComponent {
+export class SessionsListComponent implements OnInit {
+  workshopId!: number;
+  sessions!: ISession[];
 
+  constructor(
+    private sessionsService: SessionsService,
+    private activatedRoute: ActivatedRoute
+  ) {}
+
+  ngOnInit() {
+    // this.activatedRoute.snapshot.paramMap is NOT an observable unlike this.activatedRoute.paramMap which is an observable
+    const idStr = this.activatedRoute.snapshot.paramMap.get('id');
+    this.workshopId = +(idStr as string);
+
+    this.sessionsService.getSessionsForWorkshop(this.workshopId).subscribe({
+      next: (sessions) => {
+        this.sessions = sessions;
+        console.log(sessions);
+      },
+    });
+  }
 }
